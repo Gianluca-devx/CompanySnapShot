87d1b9e4c3b6f4798612c547ac7cd8bcd3008dd1
Gianluca-devx <Gianluca20.dev@gmail.com>
2026-07-06 22:27:13 +0200

    Initial Commit

---
+++ CompanySnapShot/CompanySnapShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanySnapShotApp.swift
+//  CompanySnapShot
+//
+//  Created by Gianluca Pascarella on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CompanySnapShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompanySnapShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CompanySnapShot
+//
+//  Created by Gianluca Pascarella on 06/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
